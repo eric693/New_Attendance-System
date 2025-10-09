c3f93b4d52406b249384b95edb923636e8cb50f7
eric693 <exo881222@gmail.com>
2025-10-09 09:01:02 +0800

    update

--- config.js
+++ config.js
@@ -2,9 +2,12 @@
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbzvZPwRXNBfbsi9YICkDCu3lfTRjofMth_OwViQzGVGkoba-2yunqlHQluEp3QF_qzh/exec",
+  // apiUrl: "https://script.google.com/macros/s/AKfycbzvZPwRXNBfbsi9YICkDCu3lfTRjofMth_OwViQzGVGkoba-2yunqlHQluEp3QF_qzh/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwYjIbvUjUR53VrOoEGvnGtJPZ7hWaVpvu-kOyik45P0pOT2ChRo7nUtWXtd3jDi3zRxg/exec",
+  
   // 新增回呼網址
-  redirectUrl: "https://0rigind1865-bit.github.io/Attendance-System/"
+  // redirectUrl: "https://0rigind1865-bit.github.io/Attendance-System/"
+  redirectUrl: "https://eric693.github.io/New_Attendance-System/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v1.0'
